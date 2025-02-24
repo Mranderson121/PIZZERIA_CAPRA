45efe7cf85ee5d19ff3de079525d2b013f9fb186
davide-capra <davidecapra11@gmail.com>
2025-02-24 17:13:57 +0100

    Messi come commento alcune funzionalità che non servono più

--- src/main/java/com/pizzeria/service/APIApplication.java
+++ src/main/java/com/pizzeria/service/APIApplication.java
@@ -3,15 +3,7 @@ package com.pizzeria.service;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api")
-public class APIApplication extends Application {
-	@Override
-	public Set<Class<?>> getClasses() {
-		Set<Class<?>> set = new HashSet<>();
-		set.add(TestService.class);
-		return set;
-	}
+
+public class APIApplication  {
 }
