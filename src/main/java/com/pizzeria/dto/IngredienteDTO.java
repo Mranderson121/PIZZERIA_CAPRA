be6725f4ddf60b8760855238f9bb8d42c5c892a5
davide-capra <davidecapra11@gmail.com>
2025-02-25 16:22:36 +0100

    Creata la classe DTO per gli Ingredienti

---
+++ src/main/java/com/pizzeria/dto/IngredienteDTO.java
@@ -0,0 +1,40 @@
+package com.pizzeria.dto;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class IngredienteDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int idIngrediente;
+	private String nome;
+	
+	public IngredienteDTO() {
+		
+	}
+	public IngredienteDTO(int idIngrediente, String nome) {
+		this.idIngrediente=idIngrediente;
+		this.nome=nome;
+	}
+	public int getIdIngrediente() {
+		return idIngrediente;
+	}
+	public void setIdIngrediente(int idIngrediente) {
+		this.idIngrediente = idIngrediente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+	
+
+}
