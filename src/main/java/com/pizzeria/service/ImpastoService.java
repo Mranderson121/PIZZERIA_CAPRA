394512cf40d9b10703dfb98b3417d3175e721115
davide-capra <davidecapra11@gmail.com>
2025-02-25 12:52:21 +0100

    Aggiunti tutti i metodi per la gestione delle Rest

--- src/main/java/com/pizzeria/service/ImpastoService.java
+++ src/main/java/com/pizzeria/service/ImpastoService.java
@@ -11,20 +11,45 @@ import org.springframework.stereotype.Service;
 
 import com.pizzeria.model.Impasto;
 import com.pizzeria.repository.ImpastoRepository;
+import com.pizzeria.dto.*;
 
 @Service
 @Transactional
 public class ImpastoService {
 	
 	@Autowired
-	private ImpastoRepository repo;
+	private ImpastoRepository impastoRepository;
 	
 	
 	public Set<Impasto> getAllImpasti(){
-		 return new HashSet<>(repo.findAll());
+		 return new HashSet<>(impastoRepository.findAll());
 		 }
 	public Impasto findImpastoById(int impastoId) {
-	    return repo.findById(impastoId)
+	    return impastoRepository.findById(impastoId)
 	            .orElseThrow(() -> new EntityNotFoundException("Impasto non trovato con ID: " + impastoId));
 	}
+	public ImpastoDTO modificaImpasto(int idImpasto, Impasto impasto) {
+		Impasto impastoModificato= impastoRepository.findById(idImpasto)
+				.orElseThrow(() -> new RuntimeException("Impasto non trovato"));
+		impastoModificato.setNome(impasto.getNome());
+		impastoRepository.save(impastoModificato);
+		return new ImpastoDTO(impastoModificato.getIdImpasto(),impastoModificato.getNome());
+	}
+	public Impasto aggiungiImpasto(Impasto impasto) {
+		Impasto impastoNuovo= new Impasto();
+		impastoNuovo.setNome(impasto.getNome());
+		impastoRepository.save(impastoNuovo);
+		return impastoNuovo;
+	}
+	public boolean eliminaImpasto(int impastoId) {
+		Impasto impastoEliminato=impastoRepository.findById(impastoId)
+				.orElseThrow(() -> new EntityNotFoundException("Impasto non trovato"+ impastoId));
+		if (impastoEliminato != null) {
+			impastoRepository.deleteById(impastoId);
+			return true;
+			
+		}
+		return false;
+	}
+	
 }
